70fbf403ba09391b19dc821094cd9b967ca323dd
Bri A <alexiellis22@gmail.com>
2025-02-03 13:27:27 -0500

    add styling draft and functionality to first password field

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict"
+
+const password1 = document.getElementById("password1")
+// const password2 = document.getElementById("password2")
+const submitBtn =  document.getElementById("submit-btn")
+let btnClicked = false;
+
+const characters = ["A","B","C","D","E","F","G","H","I","J","K",
+    "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+    "p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", 
+    "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%",
+    "^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",
+    ";","<",">",".","?","/"
+];
+
+function password1Gen(){
+    let password = " "
+    for (let i = 0; i < 15; i++) {
+        let index = Math.floor(Math.random()* characters.length);
+        password += characters[index]
+    }
+     password1.textContent = password
+     console.log(password);
+     
+   }
+
+
+ submitBtn.addEventListener("click", password1Gen);
